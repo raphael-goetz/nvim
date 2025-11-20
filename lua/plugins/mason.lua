e695cdcecba9b3de126d5b11517664b20d7056dc
Raphael <raphael.goetz.7@gmail.com>
2025-11-20 10:24:15 +0100

    feat: added jsonls

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,23 +1,51 @@
+vim.diagnostic.config({
+	virtual_text = true,
+	underline = true,
+	update_in_insert = true,
+})
+
+-- Start each installed LSP automatically when opening a matching file
+local function autostart_lsp(server, config)
+	vim.api.nvim_create_autocmd("FileType", {
+		pattern = "*",
+		callback = function(ev)
+			config.name = server
+			vim.lsp.start(config)
+		end,
+	})
+end
+
 return {
+	-- Mason: installs language servers
 	{
 		"williamboman/mason.nvim",
 		config = function()
 			require("mason").setup()
 		end,
 	},
+
+	-- mason-lspconfig: connects Mason → Neovim LSP system
 	{
 		"williamboman/mason-lspconfig.nvim",
+		dependencies = { "neovim/nvim-lspconfig" },
 		opts = {
-			ensure_installed = { "lua_ls", "rust_analyzer", "gopls" }
+			ensure_installed = {
+				"lua_ls",
+				"rust_analyzer",
+				"gopls",
+				"jsonls"
+			},
+			handlers = {
+				function(server)
+					-- New Neovim 0.11 API
+					local config = vim.lsp.config(server, {
+						autostart = false,             -- we autostart manually
+						settings = {},
+					})
+
+					autostart_lsp(server, config)
+				end,
+			},
 		},
 	},
-	{
-		"neovim/nvim-lspconfig",
-		config = function()
-			local lspconfig = require('lspconfig')
-			lspconfig.lua_ls.setup({})
-			lspconfig.rust_analyzer.setup({})
-			lspconfig.gopls.setup({})
-		end
-	}
 }
--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -5,6 +5,9 @@ vim.opt.relativenumber = true
 vim.opt.winborder = "rounded"
 vim.opt.undofile = true
 
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+
 vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
 vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
 vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, {})
@@ -14,16 +17,3 @@ vim.keymap.set('n', 'gr', vim.lsp.buf.references, {})
 vim.keymap.set({ 'n' }, '<leader>ca', vim.lsp.buf.code_action, {})
 vim.keymap.set({ 'n' }, '<leader>F', vim.lsp.buf.format, {})
 vim.keymap.set('n', '<leader>q', ':Neotree filesystem reveal left toggle<CR>', {})
-
---vim.lsp.config("lua_ls", require("lsp.lua_ls"))
---vim.lsp.config("rust_analyzer", require("lsp.rust_analyzer"))
-
--- Enable lua_ls
---vim.lsp.enable({ "lua_ls", "rust_analyzer" })
-
--- Diagnostic settings
-vim.diagnostic.config({
-	virtual_text = true,
-	underline = true,
-	update_in_insert = true,
-})
